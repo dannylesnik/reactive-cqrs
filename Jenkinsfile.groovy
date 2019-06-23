4232d03c0a50a8612740ea518e3adccb6d7ee24f
dannylesnik <danny.lesnik@gmail.com>
2019-06-23 10:37:02 +0300

    Jenkins POC

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -13,11 +13,18 @@ pipeline {
                 sh "${tool name: 'sbt', type: 'org.jvnet.hudson.plugins.SbtPluginBuilder$SbtInstallation'}/bin/sbt test"
             }
         }
-        stage('Docker Publish') {
-            steps {
-                sh "who am i"
-                sh "${tool name: 'sbt', type: 'org.jvnet.hudson.plugins.SbtPluginBuilder$SbtInstallation'}/bin/sbt docker:publishLocal"
-
+        stage('Publish'){
+            steps{
+                configFileProvider([
+                        configFile(fileId: 'docker-credentials', targetLocation: "${HOME}/.docker")]) {
+                    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'service.devjenkins', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                        timeout(time: 4, unit: 'MINUTES') {
+                            retry(3) {
+                                sh encoding: 'UTF-8', script: '${sbt} ecr:login'
+                            }
+                        }
+                    }
+                }
             }
         }
     }
